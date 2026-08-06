026904963326e8f6eae7f1e819493d78b2755ef6
주혁 <juhyuk@juhyeog-ui-MacBookAir.local>
2026-08-06 21:34:28 +0900

    init

---
+++ src/main/java/com/movi_backend/MoviBackendApplication.java
@@ -0,0 +1,13 @@
+package com.movi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoviBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/movi_backend/MoviBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.movi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
